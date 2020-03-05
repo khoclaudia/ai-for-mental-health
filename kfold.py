2583b9bbdcd33b2355b2fbb651a436fb695f6f18
Andey <andeyn@andrew.cmu.edu>
2020-03-05 17:25:15 -0500

    Update kfold.py

--- kfold.py
+++ kfold.py
@@ -14,13 +14,24 @@ def write_csv(df, filename):
 
     df.to_csv(filename, index=False)
 
+def writeCSV(csvData, newFileName): #creates the CSV
+    with open(newFileName, 'wt') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(csvData)
+    csvFile.close()
 
 def kfold(path):
     parsedData = pd.read_csv(path)
+    print(parsedData)
+    print(type(parsedData))
     kfoldData = []
-    arr = []
-    for i in range(1,25):
-        arr.append(i)
+    arr = ['Age', 'Gender', 'self_employed', 'family_history', 'treatment',
+           'work_interfere', 'no_employees', 'remote_work', 'tech_company', 'benefits',
+           'care_options', 'wellness_program', 'seek_help', 'anonymity', 'leave',
+           'mental_health_consequence', 'phys_health_consequence',' coworkers',
+           'supervisor', 'mental_health_interview', 'phys_health_interview',
+           'mental_vs_physical', 'obs_consequence', 'age_range']
+
     X = np.array(parsedData) #parsedData as 2D array
     y = np.array(arr) #exists for compatibilty purposes
     kf = KFold(n_splits=2) #can alter n_splits
@@ -31,8 +42,9 @@ def kfold(path):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         kfoldData.append([[X_train, X_test], [y_train, y_test]])
-
+    print(kfoldData)
     write_csv(kfoldData, 'kfoldD')
+    writeCSV(kfoldData, 'kfoldD.csv')
     return kfoldData
 
 
